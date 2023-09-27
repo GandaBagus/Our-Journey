298e29a82c657a73048487462022a6a7ffe35bf1
GandaBagus <gandabaguswibisono123@gmail.com>
2023-09-27 17:49:52 +0700

    isGround

--- Our Journey/Assets/Scripts/PlayerMovement.cs
+++ Our Journey/Assets/Scripts/PlayerMovement.cs
@@ -9,6 +9,12 @@ public class PlayerMovement : MonoBehaviour
 
     public float speed;
     public float jumpHeight;
+
+    private float dirY, dirX;
+
+    private bool isGrounded;
+
+    public bool ClimbingAllowed { get; set; }
     // Start is called before the first frame update
     private void Awake()
     {
@@ -19,23 +25,62 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     private void Update()
     {
-        float horizontalinput = Input.GetAxis("Horizontal");
-        body.velocity = new Vector2(horizontalinput * speed, body.velocity.y);
+        dirX = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(dirX * speed, body.velocity.y);
 
-        if (horizontalinput > 0.01f)
+        if (dirX > 0.01f)
         {
             transform.localScale = new Vector3(1, 1, 1);
         }
-        else if (horizontalinput < -0.01f)
+        else if (dirX < -0.01f)
         {
             transform.localScale = new Vector3(-1, 1, 1);
         }
 
-        if (Input.GetKey(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
         {
             body.velocity = new Vector2(body.velocity.x, jumpHeight);
         }
 
-        anim.SetBool("run", horizontalinput != 0);
+        /*if (ClimbingAllowed)
+        {
+            dirY = Input.GetAxisRaw("Vertical") * speed;
+        }*/
+        
+        anim.SetBool("run", dirX != 0);
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ground"))
+        {
+            isGrounded = true;
+            Debug.Log("Grounded");
+        }
+       
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Ground"))
+        {
+            isGrounded = false;
+            Debug.Log("NOT GROUNDED");
+        }
+    }
+
+    /*private void FixedUpdate()
+    {
+        
+        if (ClimbingAllowed)
+        {
+            body.isKinematic = true;
+            body.velocity = new Vector2(dirX, dirY);
+        }
+        else
+        {
+            body.isKinematic = false;
+            body.velocity = new Vector2(dirX, body.velocity.y);
+        }
+    }*/
 }
